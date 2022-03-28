added tests and fix input size calculation

model_compression_toolkit/common/graph/base_node.py
@@ -258,10 +258,11 @@ class BaseNode:
                    for candidate in self.candidates_quantization_cfg)
 
     def get_total_input_params(self):
-        total_params = 1
-        for axis_size in self.input_shape:
-            if axis_size is None:
-                continue
-            total_params *= axis_size
-        return total_params
+        """
+        Calculates the input size of the node.
+
+        Returns: input size.
+
+        """
+        return np.prod([x for x in self.input_shape if x is not None])
 