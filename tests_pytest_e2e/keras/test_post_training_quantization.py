e7bd25ea9f2efc5d9be48585b72fd660ee0bfe4d
Ofir Gordon <ofirgo@sony.com>
2024-12-05 12:42:38 +0200

    e2e pot quantization test

---
+++ tests_pytest_e2e/keras/test_post_training_quantization.py
@@ -0,0 +1,145 @@
+# Copyright 2024 Sony Semiconductor Israel, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import keras
+import pytest
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras import layers
+
+from mct_quantizers import QuantizationMethod, KerasQuantizationWrapper
+from mct_quantizers.keras.metadata import MetadataLayer
+from model_compression_toolkit.core.common.user_info import UserInformation
+from model_compression_toolkit.core.keras.constants import KERNEL, DEPTHWISE_KERNEL
+from model_compression_toolkit.ptq import keras_post_training_quantization
+from model_compression_toolkit.target_platform_capabilities.constants import KERNEL_ATTR, BIAS_ATTR
+from model_compression_toolkit.target_platform_capabilities.target_platform import OpQuantizationConfig, Signedness, \
+    AttributeQuantizationConfig
+from model_compression_toolkit.target_platform_capabilities.tpc_models.imx500_tpc.v4.tp_model import generate_tp_model
+from model_compression_toolkit.target_platform_capabilities.tpc_models.imx500_tpc.v4.tpc_keras import generate_keras_tpc
+
+INPUT_SHAPE = (224, 224, 3)
+
+
+@pytest.fixture
+def rep_data_gen():
+    np.random.seed(42)
+
+    def reppresentative_dataset():
+        for _ in range(2):
+            yield [np.random.randn(2, *INPUT_SHAPE)]
+
+    return reppresentative_dataset
+
+
+@pytest.fixture
+def model():
+    # inputs = layers.Input(shape=INPUT_SHAPE)
+    # x = layers.Conv2D(2, 3, padding='same')(inputs)
+    # x = layers.BatchNormalization()(x)
+    # x = layers.Activation('relu')(x)
+    # return tf.keras.models.Model(inputs=inputs, outputs=x)
+    return MobileNetV2()
+
+
+
+def set_tpc(weights_quantizer):
+    # TODO: we need to select a default TPC for test, which is the one we want to verify e2e for
+    #   (has all basic supported operators [TPC] and fusions and basic cfgs [TP Model])
+    #   Maybe with the new system we can save few TPC Model JSONs for different tests typs (quantization methods and mixed precision configs).
+    #   Another option is to have at least the basic configs (quant, no quant...) as test utils
+
+    att_cfg_noquant = AttributeQuantizationConfig()
+    att_cfg_quant = AttributeQuantizationConfig(weights_quantization_method=weights_quantizer,
+                                                weights_n_bits=8,
+                                                weights_per_channel_threshold=True,
+                                                enable_weights_quantization=True)
+
+    op_cfg = OpQuantizationConfig(default_weight_attr_config=att_cfg_quant,
+                                  attr_weights_configs_mapping={KERNEL_ATTR: att_cfg_quant,
+                                                                BIAS_ATTR: att_cfg_noquant},
+                                  activation_quantization_method=QuantizationMethod.POWER_OF_TWO,
+                                  activation_n_bits=8,
+                                  supported_input_activation_n_bits=8,
+                                  enable_activation_quantization=False,  # No activation quantization
+                                  quantization_preserving=False,
+                                  fixed_scale=None,
+                                  fixed_zero_point=None,
+                                  simd_size=32,
+                                  signedness=Signedness.AUTO)
+
+    tpm = generate_tp_model(default_config=op_cfg, base_config=op_cfg, mixed_precision_cfg_list=[op_cfg],
+                            name="test_tpm")
+    tpc = generate_keras_tpc(name='test_tpc', tp_model=tpm)
+    return tpc
+
+
+@pytest.fixture
+def tpc_factory():
+    def _tpc_factory(weights_quantizer):
+        return set_tpc(weights_quantizer)
+    return _tpc_factory
+
+
+class TestPostTrainingQuantizationApi:
+    # TODO:
+    #   [V] TPC - extract from test (local)
+    #   [V] rep dataset
+    #   [] model - use real model (MBV2)? or at least more complicated model structure and layers.
+    #   [] [pot, symmetric, uniform] X [w, a, w&a]
+    #   ! Features are e2e-tested separately (folding, bc, snc...)
+
+    @pytest.mark.parametrize("weights_quantizer", [QuantizationMethod.POWER_OF_TWO])
+    def test_ptq_pot_weights_only(self, model, rep_data_gen, tpc_factory, weights_quantizer):
+
+        tpc = tpc_factory(weights_quantizer)
+        q_model, quantization_info = keras_post_training_quantization(model, rep_data_gen,
+                                                                      target_platform_capabilities=tpc)
+
+        assert q_model is not None and isinstance(q_model, keras.Model)
+        assert quantization_info is not None and isinstance(quantization_info, UserInformation)
+
+        # Assert quantized model structure
+        assert len([l for l in q_model.layers if isinstance(l, layers.BatchNormalization)]) == 0, \
+            "Expects BN folding in quantized model."
+        assert len([l for l in q_model.layers if isinstance(l, MetadataLayer)]) == 1, \
+            "Expects quantized model to have a metadata stored in a dedicated layer."
+        # original_conv_layers = [l for l in model.layers if isinstance(l, layers.Conv2D)]
+        original_conv_layers = [l for l in model.layers if isinstance(l, (layers.Conv2D, layers.DepthwiseConv2D, layers.Dense))]
+        quantized_conv_layers = [l for l in q_model.layers if isinstance(l, KerasQuantizationWrapper)]
+        assert len(original_conv_layers) == len(quantized_conv_layers), \
+            "Expects all conv layers from the original model to be wrapped with a KerasQuantizationWrapper."
+
+        # Assert quantization properties
+        for kqw in quantized_conv_layers:
+            # assert isinstance(kqw.layer, layers.Conv2D)
+            assert isinstance(kqw.layer, (layers.Conv2D, layers.DepthwiseConv2D, layers.Dense))
+
+            if isinstance(kqw.layer, layers.DepthwiseConv2D):
+                conv_quantizer = kqw.weights_quantizers[DEPTHWISE_KERNEL]
+                num_output_chnnels = kqw.layer.depthwise_kernel.shape[-1] * kqw.layer.depthwise_kernel.shape[-2]
+            else:
+                conv_quantizer = kqw.weights_quantizers[KERNEL]
+                num_output_chnnels = kqw.layer.kernel.shape[-1]
+
+            assert conv_quantizer.per_channel
+            assert conv_quantizer.quantization_method[0] == QuantizationMethod.POWER_OF_TWO
+            assert len(conv_quantizer.threshold) == num_output_chnnels
+            for t in conv_quantizer.threshold:
+                assert np.log2(np.abs(t)).astype(int) == np.log2(np.abs(t))
+                
+
+
